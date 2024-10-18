425028fdf1954ca57f17c15c4b28788ef12550f1
Saul <exmosaul@gmail.com>
2024-10-18 11:14:29 +0100

    Ejercicio finalizado con el deserializer para todos los atributos de Title

--- src/main/java/Title.java
+++ src/main/java/Title.java
@@ -1,4 +1,8 @@
-public record Title(String id, Title.TitleType titleType, String primaryTitle) {
+import java.time.Year;
+import java.util.List;
+
+public record Title(String id, Title.TitleType titleType, String primaryTitle, String originalTitle, boolean isAdult,
+                    Year startYear, Year endYear, Integer runtimeMinutes, List<Genres> genres) {
 
     public enum TitleType {
         VIDEOGAME,
@@ -12,5 +16,36 @@ public record Title(String id, Title.TitleType titleType, String primaryTitle) {
         VIDEO,
         TVMOVIE,
         TVEPISODE
+    }
+
+    public enum Genres{
+        Action,
+        Adult,
+        Adventure,
+        Animation,
+        Biography,
+        Comedy,
+        Crime,
+        Documentary,
+        Drama,
+        Family,
+        Fantasy,
+        Film_Noir,
+        Game_Show,
+        History,
+        Horror,
+        Music,
+        Musical,
+        Mystery,
+        News,
+        Reality_TV,
+        Romance,
+        Sci_Fi,
+        Short,
+        Sport,
+        Talk_Show,
+        Thriller,
+        War,
+        Western
     }
 }
--- src/main/java/TsvTitleDeserializer.java
+++ src/main/java/TsvTitleDeserializer.java
@@ -1,7 +1,32 @@
+import java.time.Year;
+import java.util.ArrayList;
+import java.util.List;
+
 public class TsvTitleDeserializer implements TitleDeserializer{
     @Override
     public Title deserialize(String value) {
         String[] columns = value.split("\t");
-        return new Title(columns[0], Title.TitleType.valueOf(columns[1].toUpperCase()), columns[2]);
+        return new Title(columns[0], Title.TitleType.valueOf(columns[1].toUpperCase()), columns[2], columns[3],
+                Boolean.parseBoolean(columns[4]), getYear(columns[5]), getYear(columns[6]), getMinutes(columns[7]),
+                getGenres(columns[8]));
+    }
+
+    public Integer getMinutes(String s){
+        if(s.equals("\\N")) return null;
+        return Integer.parseInt(s);
+    }
+
+    public Year getYear(String s){
+        if(s.equals("\\N")) return null;
+        return Year.parse(s);
+    }
+
+    public List<Title.Genres> getGenres(String s){
+        if(s.equals("\\N")) return null;
+        s = s.replace("-", "_");
+        String[] genres = s.split(",");
+        List<Title.Genres> genresList = new ArrayList<>();
+        for(String g:genres) genresList.add(Title.Genres.valueOf(g));
+        return genresList;
     }
 }
