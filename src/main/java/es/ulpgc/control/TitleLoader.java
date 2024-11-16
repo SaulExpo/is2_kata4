c756fd1a7a9de1bfaef2608e2ca9f39962e4088e
Saul <exmosaul@gmail.com>
2024-11-16 11:35:38 +0000

    Creación de la clase TitleLoader

---
+++ src/main/java/es/ulpgc/control/TitleLoader.java
@@ -0,0 +1,19 @@
+package es.ulpgc.control;
+
+import es.ulpgc.model.Title;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+
+public class TitleLoader {
+
+    public void loadTitles(File source, File target) throws IOException {
+        TitleReader reader = new TsvTitleReader(source);
+        SQLiteTitleWriter dbWriter = new SQLiteTitleWriter(target);
+        Iterator<Title> titles = reader.read();
+        while (titles.hasNext()){
+            dbWriter.write(titles.next());
+        }
+    }
+}
